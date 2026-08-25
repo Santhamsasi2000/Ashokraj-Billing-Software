7a8534221f569d8f35ba431d642dbac08271aa40
Santhamsasi2000 <santhamsasi68670@gmail.com>
2026-08-25 09:25:37 +0530

    New Changes

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: "http://localhost:5000/api",
+  baseURL: "/api",
 });
 
 export const getMenuItems = (period) => API.get(`/menu?period=${period}`);
--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -2,10 +2,17 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 
-// https://vite.dev/config/
 export default defineConfig({
-  base: "/",
   plugins: [
     react(),
-    tailwindcss(),],
+    tailwindcss()
+  ],
+  server: {
+    proxy: {
+      '/api': {
+        target: 'http://localhost:5000', // Redirects all /api requests to Express
+        changeOrigin: true,
+      }
+    }
+  }
 })
